7389735e6f1471b49ad8d004353254d25b4d295c
benzt2h-nb <benzt2h@hotmail.com>
2018-08-09 21:01:44 +0700

    fix api

--- map_api.php
+++ map_api.php
@@ -11,19 +11,20 @@ $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
 $sql = "SELECT * FROM map";
 $query = mysqli_query($conn,$sql);
 #### array
-$json_return = array();
+
+$map_arr=array();
+$map_arr["data"]=array();
  while ($row=mysqli_fetch_array($query,MYSQLI_ASSOC)) {
- array_push($json_return,
- array(
+ $json_return=array(
  "map_number" => $row["map_number"],
  "map_name" => $row["map_name"],
  "map_description" => $row["map_description"],
  "map_img" => "http://www.lmtznetwork.info/backend-snru/".$row["map_img"],
  "map_latitude" => $row["map_latitude"],
  "map_logitude" => $row["map_logitude"]
- )
  );
+array_push($map_arr["data"],$json_return);
  }
-
- echo json_encode($json_return);
+ 
+ echo json_encode($map_arr);
 ?>
--- news_api.php
+++ news_api.php
@@ -11,17 +11,18 @@ $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
 $sql = "SELECT * FROM news";
 $query = mysqli_query($conn,$sql);
 #### array
+
 $json_return = array();
+$json_return["data"]=array();
  while ($row=mysqli_fetch_array($query,MYSQLI_ASSOC)) {
- array_push($json_return,
- array(
+array_push($json_return["data"],array(
  "news_number" => $row["news_number"],
  "news_header" => $row["news_header"],
  "news_description" => $row["news_description"],
  "news_img" => "http://www.lmtznetwork.info/backend-snru/".$row["news_img"]
- )
+)
  );
  }
- $json_re = array_reverse($json_return);
- echo json_encode($json_re);
+ $reverse["data"] = array_reverse($json_return["data"]);
+ echo json_encode($reverse);
 ?>
